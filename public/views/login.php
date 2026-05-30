8d022e816a2da81985398e58cb61e704f357a1b6
Fernandodg97 <ferdiaz1997@gmail.com>
2026-05-30 03:42:03 +0200

    feat: Se soluciona error donde se guarda jwt en una cookie en login

--- public/views/login.php
+++ public/views/login.php
@@ -9,7 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = htmlspecialchars($_POST["password"]);
 
     if (SessionController::userLogIn($username, $password)) {
-        $_SESSION['jwt'] = $_COOKIE['jwt'];
         redirect("/admin");
     } else {
         // Si el login falla, establecer un error en la sesión
